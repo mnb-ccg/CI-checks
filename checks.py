699ce2d1d0b91911042638efe5a251217e5bc01e
Mathias Niebuhr Bjerregaard <mnb.ccg@cbs.dk>
2021-10-07 15:37:04 +0200

    requirements

--- checks.py
+++ checks.py
@@ -55,6 +55,8 @@ def column_check(df):
     correct_col = ['company_id', 'company_name', 'year', 'person_id', 'name', 'annual_report', 'birth_date', 'gender', 'nationality', 'role code', 'position', 'election_form', 'independent', 'on_board', 'appointment_date', 'committee - audit', 'committee - nomination', 'committee - remuneration', 'committee - risk', 'step_down', 'months serving', 'comment', 'current_shares_a_number', 'current_shares_a_value', 'current_shares_b_number', 'current_shares_b_value', 'current_shares_total_number', 'current_shares_total_value', 'current_options_number', 'current_options_value', 'currency', 'base salary', 'bonus', 'total', 'award_total_value', 'award_options', 'award_options_number', 'award_options_value', 'award_shares', 'award_performance', 'award_restricted', 'award_tot_shares_value', 'award_tot_shares_number', 'pension_total', 'other_monetary', 'other_non_monetary']
     correct_col_clean = col_standardize(correct_col)
     
+    st.write(df_col_clean)
+    st.write(correct_col)
      
     if (df_col_clean[0:41] == correct_col_clean):
         correct = True
