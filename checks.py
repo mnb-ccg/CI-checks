d5875a2a42ab8e186237f9911d99151fc4a4ceef
Mathias Niebuhr Bjerregaard <mnb.ccg@cbs.dk>
2021-10-12 12:32:54 +0200

    ...

--- checks.py
+++ checks.py
@@ -53,8 +53,8 @@ def column_check(df):
     df_col_clean = col_standardize(df_col)
     df_col_clean = df_col_clean[0:46]
     
-    correct_col = ['company_id', 'company_name', 'year', 'person_id', 'name', 'annual_report', 'birth_date', 'gender', 'nationality', 'role code', 'position', 'election_form', 'independent', 'on_board', 'appointment_date', 'committee - audit', 'committee - nomination', 'committee - remuneration', 'committee - risk', 'step_down', 'months serving', 'comment', 'current_shares_a_number', 'current_shares_a_value', 'current_shares_b_number', 'current_shares_b_value', 'current_shares_total_number', 'current_shares_total_value', 'current_options_number', 'current_options_value', 'currency', 'base salary', 'bonus', 'total', 'award_total_value', 'award_options', 'award_options_number', 'award_options_value', 'award_shares', 'award_performance', 'award_restricted', 'award_tot_shares_value', 'award_tot_shares_number', 'pension_total', 'other_monetary', 'other_non_monetary']
-    correct_col_clean = col_standardize(correct_col)
+    correct_col = ['company_id', 'company_name', 'year', 'person_id', 'name', 'annual_report', 'birth_date', 'gender', 'nationality', 'role_code', 'position', 'election_form', 'independent', 'on_board', 'appointment_date', 'nomination', 'audit', 'remuneration', 'risk', 'step_down', 'months_serving', 'comment', 'current_shares_A_number', 'current_shares_A_value', 'current_shares_B_number', 'current_shares_B_value', 'current_shares_total_number', 'current_shares_total_value', 'current_options_number', 'current_options_value', 'currency', 'base_salary', 'bonus', 'total', 'award_total_value', 'award_options', 'award_shares', 'award_performance', 'award_restricted', 'pension_total', 'other_monetary', 'other_non_monetary', 'row_id']
+    correct_col_clean = col_standardize(column_names)
     
 
     if (df_col_clean == correct_col_clean):
