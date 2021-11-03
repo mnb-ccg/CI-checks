b8d8012e913cfaa5b97e1213a41cfed223d8f8cc
Mathias Niebuhr Bjerregaard <mnb.ccg@cbs.dk>
2021-11-03 16:03:40 +0100

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,8 @@ with type_check:
         if(type_c != 'str' and type_c != 'no check'):
             df_check = ch.check_type(df_i, column, type_c)
             df_type_check[column] = df_check
-    st.write(df_type_check.shape)
+    for i in range(df_type_check.shape[1]):
+        st.write(i)
     #df_type_check['feedback'] = df_type_check[2:]
             
     
