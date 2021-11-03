2df7c021e8e8c4f5594daed2fea0c88f887ad827
Mathias Niebuhr Bjerregaard <mnb.ccg@cbs.dk>
2021-11-03 18:14:31 +0100

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,6 +119,7 @@ with birth_date_check:
     bool_cbd = False 
     if(column_check_i & column_check_c):
         df_check = ch.check_date(df_i, 'birth_date')
+        st.write(df_check)
         df_type_check['feedback'] = df_type_check['feedback'] + df_check.to_frame()['birth_date']
 
 
